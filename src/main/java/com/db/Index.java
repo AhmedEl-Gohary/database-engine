b516fcc3c4950ea39c193342a0358397da95b958
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2024-04-19 16:00:21 +0200

    add index (tested)

--- src/main/java/com/db/Index.java
+++ src/main/java/com/db/Index.java
@@ -19,8 +19,6 @@ public class Index<TKey extends Comparable<TKey>> implements Serializable {
         build();
     }
 
-
-
     private void build() {
         Table tableInstance = (Table) DBApp.fnDeserialize(strTableName);
         for (String page : tableInstance.vecPages) {
