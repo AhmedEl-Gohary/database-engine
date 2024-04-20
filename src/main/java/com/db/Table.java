e520e13c0471df7d7e19b2192cf652ca196dc186
AhmedKamal18 <ahmedelbakly95@gmail.com>
2024-04-20 18:18:39 +0200

    fix bs bug

--- src/main/java/com/db/Table.java
+++ src/main/java/com/db/Table.java
@@ -35,7 +35,8 @@ public class Table implements Serializable{
         if (htblColNameValue.get(this.strClusteringKeyColumn) == null) {
             throw new DBAppException("Clustering Key cannot be null!");
         }
-        int iPageNumber = fnBSPageLocation((Comparable) htblColNameValue.get(this.strClusteringKeyColumn));
+        int iPageNumber = fnGetPageLocation((Comparable) htblColNameValue.get(this.strClusteringKeyColumn));
+        if (iPageNumber == - 1) iPageNumber = 0;
         Entry entryInstance = new Entry(htblColNameValue, this.strClusteringKeyColumn);
         Hashtable<String, String> colIndicesNames = Meta.fnMapColumnToIndexName(strTableName);
         Hashtable<String, Index> colIndices = new Hashtable<>();
@@ -70,7 +71,8 @@ public class Table implements Serializable{
             throw new DBAppException("Clustering Key cannot be null!");
         }
         Comparable cmpClusteringKey = (Comparable) htblColNameValue.get(strClusteringKeyColumn);
-        int iPageNumber = fnBSPageLocation(cmpClusteringKey);
+        int iPageNumber = fnGetPageLocation(cmpClusteringKey);
+        if (iPageNumber == -1) iPageNumber = 0;
         Page pageBlock = (Page) DBApp.deserialize(vecPages.get(iPageNumber));
         Entry entryTuple = new Entry(htblColNameValue, strClusteringKeyColumn);
         int iEntryIdx = Collections.binarySearch(pageBlock.vecTuples, entryTuple);
@@ -104,7 +106,7 @@ public class Table implements Serializable{
 //        if (htblColNameValue.get(this.strClusteringKeyColumn) == null) {
 //            throw new DBAppException("Clustering Key cannot be null!");
 //        }
-//        int iPageNumber = fnBSPageLocation((Comparable) htblColNameValue.get(this.strClusteringKeyColumn));
+//        int iPageNumber = fnGetPageLocation((Comparable) htblColNameValue.get(this.strClusteringKeyColumn));
 //        Page pageBlock = (Page) DBApp.fnDeserialize(vecPages.get(iPageNumber));
 //        Entry entryTuple = new Entry(htblColNameValue, strClusteringKeyColumn);
 //        int iEntryIdx = Collections.binarySearch(pageBlock.vecTuples, entryTuple);
@@ -123,21 +125,21 @@ public class Table implements Serializable{
         return vecCountRows.get(iPageIdx) == Page.iMaxRowsCount;
     }
 
-    public int fnBSPageLocation(Comparable oTarget){
-        int N = vecPages.size();
-        int l = 0, r = N - 1;
-        int iFirstGoodIdx = 0;
-        while (l <= r) {
-            int mid = l + r >> 1;
-            if (oTarget.compareTo(vecMin.get(mid)) >= 0) {
-                iFirstGoodIdx = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return iFirstGoodIdx;
-    }
+//    public int fnGetPageLocation(Comparable oTarget){
+//        int N = vecPages.size();
+//        int l = 0, r = N - 1;
+//        int iFirstGoodIdx = 0;
+//        while (l <= r) {
+//            int mid = l + r >> 1;
+//            if (oTarget.compareTo(vecMin.get(mid)) >= 0) {
+//                iFirstGoodIdx = mid;
+//                r = mid - 1;
+//            } else {
+//                l = mid + 1;
+//            }
+//        }
+//        return iFirstGoodIdx;
+//    }
 
     public int fnGetPageLocation(Comparable oTarget){
         int N = vecPages.size();
@@ -156,7 +158,7 @@ public class Table implements Serializable{
     }
 
     public void fnDeleteEntry(Entry entry){
-        int iPageNumber = fnBSPageLocation(entry.fnEntryID());
+        int iPageNumber = fnGetPageLocation(entry.fnEntryID());
         Page pageInstance = (Page) DBApp.deserialize(vecPages.get(iPageNumber));
         int iEntryIdx = Collections.binarySearch(pageInstance.vecTuples, entry);
         if (iEntryIdx >= 0){
@@ -176,7 +178,8 @@ public class Table implements Serializable{
 
     public String fnGetKeyPage(Comparable oTarget){
         if (isEmpty()) return null;
-        int iPageNumber = fnBSPageLocation(oTarget);
+        int iPageNumber = fnGetPageLocation(oTarget);
+        if (iPageNumber == -1) iPageNumber = 0;
         return vecPages.get(iPageNumber);
     }
 
